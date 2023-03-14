3c30da1b570322180b581899ce4588a4a0398c44
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-14 18:58:23 +0530

    Add: UC1-ValidFirstName

---
+++ UserRegistrationproblemUsingRegex/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationproblemUsingRegex
+{
+    internal class Pattern
+    {
+        public static string Regex_Rule = "^[A-Z][A-Za-z]{3,9}$";
+
+        public bool validatePinCode(string inputString)
+        {
+            return Regex.IsMatch(inputString, Regex_Rule);
+        }
+    }
+}
+
--- UserRegistrationproblemUsingRegex/Program.cs
+++ UserRegistrationproblemUsingRegex/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("....Welcome to User Registation Problem Using Regex.....!");
+            Pattern pattern = new Pattern();
+            if (pattern.validatePinCode("Pallavi"))//Valid name
+            //if (pattern.validatePinCode("pallaviathawale"))//invalid name
+                {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
     }
+    
 }
