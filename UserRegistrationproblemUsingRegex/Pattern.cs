85c63108fdb49ed87f8124ff6df757cb1d5cfe52
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-22 09:49:24 +0530

    Add: UC5-PasswordRuleOne

--- UserRegistrationproblemUsingRegex/Pattern.cs
+++ UserRegistrationproblemUsingRegex/Pattern.cs
@@ -9,12 +9,20 @@ namespace UserRegistrationproblemUsingRegex
 {
     internal class Pattern
     {
-        public static string Regex_Rule = "^[+][1-9]{2}[ ][6-9][0-9]{9}$";
+        // public static string Regex_Rule = "^[9][1][ ][6-9][0-9]{9}$";// phon number
+        // public static string Regex_Rule = "^[A-Za-z]{6}[0-9][!@#$%^&]@$";//password
+        public static string Regex_Rule = "^[a-zA-Z0-9]{8,}$";//UC5-8characters
+        // public static string Regex_Rule = "^(?=.*[A-Z]).{8,}$"; //UC6-at least one uppar case
+        //  public static string Regex_Rule = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%_]{8,}$";//UC7-one numeric number
+       // public static string Regex_Rule = "^^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%_])[a-zA-Z0-9!@#$%_]{8,}$"; //UC8-At least one special characters
+        // public static string Regex_Rule = "^.{0,7}|[^A-Z]|[^0-9]$"
 
-        public bool validatePinCode(string inputString)
+        public bool Inputvalidate(string inputString)
         {
             return Regex.IsMatch(inputString, Regex_Rule);
         }
+
+       
     }
 }
 
--- UserRegistrationproblemUsingRegex/Program.cs
+++ UserRegistrationproblemUsingRegex/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Pattern pattern = new Pattern();
-            if (pattern.validatePinCode("+91 7218533707"))//Valid phon number
-            //if (pattern.validatePinCode("+91 7218533399"))//invalid phon number
+           // if (pattern.validatePinCode("Pallav2@"))//invalid password
+           if (pattern.Inputvalidate("Pallav22")) //valid 
                 {
                 Console.WriteLine("Valid");
             }
